5711bf6b654a5bd94b92380e9b7a3ef7232371b4
MarkACohen <mark@cassconsulting.biz>
2017-02-02 10:12:21 -0500

    adding stat alg

---
+++ 02-Interfaces/StatAlg/CompositeAlg.java
@@ -0,0 +1,25 @@
+package edu.mcla.cs.statalg;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompositeAlg implements IStatAlg
+{
+    private List<IStatAlg> m_algorithms = new ArrayList<IStatAlg>();
+    
+    @Override
+    public List<Double> process(List<Double> data)
+    {
+        for (IStatAlg alg : m_algorithms)
+        {
+            data = alg.process(data);
+        }
+        return data;
+    }
+    
+    public void addAlg(IStatAlg alg)
+    {
+        m_algorithms.add(alg);
+    }
+
+}
---
+++ 02-Interfaces/StatAlg/IStatAlg.java
@@ -0,0 +1,8 @@
+package edu.mcla.cs.statalg;
+
+import java.util.List;
+
+public interface IStatAlg
+{
+    public List<Double> process(List<Double> data);
+}
